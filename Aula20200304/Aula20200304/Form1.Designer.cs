c5c16977f587ed73adedcb9a4cf8cefc7485baa1
gustavovalentim <43221033+gustavovalentim@users.noreply.github.com>
2020-03-05 10:09:15 -0300

    Add files via upload

---
+++ Aula20200304/Aula20200304/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Aula20200304
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOi = new System.Windows.Forms.Button();
+            this.btnModelo2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOi
+            // 
+            this.btnOi.Location = new System.Drawing.Point(116, 58);
+            this.btnOi.Name = "btnOi";
+            this.btnOi.Size = new System.Drawing.Size(75, 23);
+            this.btnOi.TabIndex = 0;
+            this.btnOi.Text = "Oi";
+            this.btnOi.UseVisualStyleBackColor = true;
+            this.btnOi.Click += new System.EventHandler(this.btnOi_Click);
+            // 
+            // btnModelo2
+            // 
+            this.btnModelo2.Location = new System.Drawing.Point(116, 106);
+            this.btnModelo2.Name = "btnModelo2";
+            this.btnModelo2.Size = new System.Drawing.Size(75, 23);
+            this.btnModelo2.TabIndex = 1;
+            this.btnModelo2.Text = "Modelo2";
+            this.btnModelo2.UseVisualStyleBackColor = true;
+            this.btnModelo2.Click += new System.EventHandler(this.btnModelo2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnModelo2);
+            this.Controls.Add(this.btnOi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOi;
+        private System.Windows.Forms.Button btnModelo2;
+    }
+}
+
---
+++ Aula20200304/Aula20200304/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gurobi;
+using System.Diagnostics;
+
+namespace Aula20200304
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOi_Click(object sender, EventArgs e)
+        {
+            GRBEnv Ambiente = new GRBEnv();
+            GRBModel Modelo = new GRBModel(Ambiente);
+            GRBVar x = Modelo.AddVar(0, 1, 0, GRB.BINARY, "x");
+            GRBVar y = Modelo.AddVar(0, 1, 0, GRB.BINARY, "y");
+            GRBVar z = Modelo.AddVar(0, 1, 0, GRB.BINARY, "z");
+            Modelo.SetObjective(x + y + 2 * z, GRB.MAXIMIZE);
+            Modelo.AddConstr(x + 2 * y + 3 * z <= 4.0, "c0");
+            Modelo.AddConstr(x + y >= 1.0, "c1");
+            Modelo.Write("C:\\Teste\\ModeloAula.lp");
+            Modelo.Optimize();
+            MessageBox.Show("O valor da variável x é: " + x.X.ToString());
+            MessageBox.Show("O valor da variável y é: " + y.X.ToString());
+            MessageBox.Show("O valor da variável z é: " + z.X.ToString());
+
+        }
+
+        private void btnModelo2_Click(object sender, EventArgs e)
+        {
+            GRBEnv Ambinte = new GRBEnv();
+            GRBModel Modelo = new GRBModel(Ambinte);
+            Random Aleatorio = new Random(4);
+            int m = 1600;
+            GRBVar[,] X = new GRBVar[m, m];
+            for(int i=0;i<m;i++)
+            {
+                for(int j=0;j<m;j++)
+                {
+                    X[i, j] = Modelo.AddVar(0, 1, Aleatorio.Next(1, 20), GRB.BINARY, "x_" + i.ToString() + "_" + j.ToString());
+                }
+            }
+            Modelo.ModelSense = GRB.MAXIMIZE;
+            GRBLinExpr Expressao=new GRBLinExpr();
+            for (int i=0;i<m;i++)
+            {
+                Expressao.Clear();
+                for (int j = 0; j < m; j++)
+                {
+                    Expressao.AddTerm(1, X[i, j]);
+                }
+                Modelo.AddConstr(Expressao == 1, "Vendedor_" + i);
+            }
+
+            for (int j = 0; j < m; j++)
+            {
+                Expressao.Clear();
+                for (int i = 0; i < m; i++)
+                {
+                    Expressao.AddTerm(1, X[i, j]);
+                }
+                Modelo.AddConstr(Expressao == 1, "Regiao_" + j);
+            }
+            Stopwatch Cronometro = new Stopwatch();
+            Cronometro.Start();
+            Modelo.Optimize();
+            Cronometro.Stop();
+            MessageBox.Show("O valor do lucro é: " + Modelo.ObjVal.ToString());
+            MessageBox.Show("O tempo para resolver foi de: " + Cronometro.ElapsedMilliseconds.ToString() + " ms");
+            MessageBox.Show("Se quiser saber a alocação que gera esse lucro é só me pagar");
+            //for (int j = 0; j < m; j++)
+            //{
+            //    for (int i = 0; i < m; i++)
+            //    {
+            //        if(X[i,j].X>0)
+            //        {
+            //            MessageBox.Show("O vendedor " + i + " é alocado para a região " + j);
+            //        }
+            //    }
+            //}
+            Modelo.Write("C:\\Teste\\Modelo2.lp");
+        }
+    }
+}
